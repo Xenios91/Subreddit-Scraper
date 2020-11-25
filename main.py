b7ef984e3cf570466121c2652ee73b82f5f20836
xenios <xenios91@protonmail.com>
2020-11-24 21:15:17 -0600

    added autoscroll and writing html to a text file

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.webdriver.firefox.options import Options
 import time
 
 main_url = "https://www.reddit.com/r/politics/"
+file_name = "reddit_politics.html"
 
 
 def get_webdriver() -> str:
@@ -20,25 +21,36 @@ def get_selenium_options() -> Options:
     #options.set_headless()
     return options
 
+def scroll_to_bottom(count: int, driver: webdriver):
+    for _ in range(count):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(2)
 
 def get_html(driver: webdriver, url: str) -> str:
     driver.get(url)
-    time.sleep(5)
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(5)
+    scroll_to_bottom(10, driver)
     html = driver.page_source
     return html
 
+def write_to_file(data: str) -> bool:
+    try:
+        file = open(file_name, "w")
+        file.write(data)
+        file.close()
+    except Exception as e:
+        print(e)
 
 def main():
     driver = get_webdriver()
     response = get_html(driver, main_url)
     soup = BeautifulSoup(response, 'lxml')
-    links = soup.findAll("article")
-    for link in links:
-        href = link.attrs.get("href")
-        if "comments" in href:
-            pass
+    soup = soup.prettify()
+    success = write_to_file(soup)
+    if success:
+        print("Done scraping {0}".format(main_url))
+    else:
+        print("Scraping {0} failed".format(main_url))
+
 
 
 main()

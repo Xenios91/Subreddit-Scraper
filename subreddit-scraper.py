b9d6cbafcca942c89847bcfac7a05835ae98af46
xenios <xenios91@protonmail.com>
2020-11-26 23:25:20 -0600

    default filename changes

--- subreddit-scraper.py
+++ subreddit-scraper.py
@@ -7,7 +7,7 @@ def get_args() -> list:
     parser.add_argument(
         "--subreddit", type=str, help="The subreddit to scrap. Example: --subreddit ProgrammerHumor", required=True)
     parser.add_argument(
-        "--output", type=str, help="The filename to save the source to. Default is reddit_scrap_urls. Example: --output reddit_data", required=False, default="reddit_scrap_urls")
+        "--output", type=str, help="The filename to save the source to. Default is reddit_[subreddit]_urls. Example: --output reddit_data", required=False, default="reddit_[subreddit]_urls")
     parser.add_argument(
         "--cid", type=str, help="Your client ID. Example: --cid=47djfhshg3756j", required=True)
     parser.add_argument(
@@ -17,14 +17,19 @@ def get_args() -> list:
     args = parser.parse_args()
     return args
 
+def change_file_name(filename: str, subreddit: str) -> str:
+    if "[subreddit]" in filename:
+        filename = filename.replace("[subreddit]", subreddit)
+    return filename
 
 def main():
     args = get_args()
+    file_name = change_file_name(args.output, args.subreddit)
     reddit_url_scraper = subreddit_utils.SubredditURLScraper(
         args.cid, args.cs)
     dataframe = reddit_url_scraper.scrap_subreddit(args.subreddit, args.limit)
     subreddit_utils.DataFrameUtils.write_csv_to_file(
-        args.output, ",", dataframe)
+        file_name, ",", dataframe)
 
 
 main()

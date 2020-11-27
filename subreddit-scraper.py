af1d0d5b700bf810859c2cd9929f11c205eb21eb
xenios <xenios91@protonmail.com>
2020-11-26 22:57:20 -0600

    changed to use praw

--- subreddit-scraper.py
+++ subreddit-scraper.py
@@ -1,97 +1,30 @@
-from bs4 import BeautifulSoup
 import argparse
-import requests
-import time
-
-main_message = """
- _____       _                  _     _ _ _          _____
-/  ___|     | |                | |   | (_) |        /  ___|
-\ `--. _   _| |__  _ __ ___  __| | __| |_| |_ ______\ `--.  ___ _ __ __ _ _ __   ___ _ __
- `--. \ | | | '_ \| '__/ _ \/ _` |/ _` | | __|______|`--. \/ __| '__/ _` | '_ \ / _ \ '__|
-/\__/ / |_| | |_) | | |  __/ (_| | (_| | | |_       /\__/ / (__| | | (_| | |_) |  __/ |
-\____/ \__,_|_.__/|_|  \___|\__,_|\__,_|_|\__|      \____/ \___|_|  \__,_| .__/ \___|_|
-                                                                         | |
-                                                                         |_|
-More info: https://github.com/Xenios91/Subreddit-Scraper
-"""
-
-
-def get_next_url(html: str) -> str:
-    soup = BeautifulSoup(html, "lxml")
-    elements = soup.findAll("span", {"class": "next-button"})
-    for element in elements:
-        contents = element.contents[0]
-        if "next" in contents.next:
-            attrs = contents.attrs
-            href = attrs.get("href")
-            return href
-    return "END"
-
-
-def traverse_pages(url: str, page_count: int, file_name: str):
-    traverse = True
-    counter = 1
-    while traverse:
-        print("Evaluating page {0}".format(counter), end='\r')
-        try:
-            headers = {
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-            response = requests.get(url, headers=headers)
-            html = response.text
-            retrieve_all_post_urls(url, html, file_name)
-            url = get_next_url(html)
-            time.sleep(5) #sleep 5 seconds to help prevent throttling.
-            if url == "END":
-                return
-        except Exception as e:
-            print(e)
-        counter = counter + 1
-        if counter > page_count and page_count != -1:
-            traverse = False
+import subreddit_utils
 
 
 def get_args() -> list:
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-url", type=str, help="The subreddit to scrap. Example: -url https://www.reddit.com/r/ProgrammerHumor/", required=True)
+        "--subreddit", type=str, help="The subreddit to scrap. Example: --subreddit ProgrammerHumor", required=True)
     parser.add_argument(
-        "-output", type=str, help="The filename to save the source to. Default is reddit_scrap_urls. Example: -output reddit_data", required=False, default="reddit_scrap_urls")
+        "--output", type=str, help="The filename to save the source to. Default is reddit_scrap_urls. Example: --output reddit_data", required=False, default="reddit_scrap_urls")
     parser.add_argument(
-        "-pages", type=int, help="The number of pages to traverse, if -1 is used all pages will be traversed, the default is 10. Example -pages 200", required=False, default=10)
+        "--cid", type=str, help="Your client ID. Example: --cid=47djfhshg3756j", required=True)
+    parser.add_argument(
+        "--cs", type=str, help="Your client secret. Example: --cs=nvb67462nfFF58221fzaa", required=True)
+    parser.add_argument(
+        "--limit", type=int, help="The amount of post you want to scrap, if -1 is used it will pull all data available. Example: --limit 100. The Default is 10", required=False, default=10)
     args = parser.parse_args()
     return args
 
 
-def retrieve_all_post_urls(url: str, html: str, file_name: str):
-    file_name = "{0}{1}".format(file_name, "_urls")
-    with open(file_name, "a") as file:
-        soup = BeautifulSoup(html, 'lxml')
-        links = soup.findAll("a", {"class": "comments"})
-        for link in links:
-            href = link.attrs.get("href")
-            url_to_write = "{0}{1}".format(href, "\n")
-            file.write(url_to_write)
-
-
-def update_url(url: str) -> str:
-    url = url.replace("www", "old")
-    return url
-
-
 def main():
-    print(main_message)
     args = get_args()
-    output_file = args.output
-    url = args.url
-    pages = args.pages
-
-    try:
-        url = update_url(args.url)
-        print("Starting web scraping!")
-        traverse_pages(url, pages, output_file)
-        print("Done scraping {0}".format(url))
-    except Exception:
-        print("An error has occured")
+    reddit_url_scraper = subreddit_utils.SubredditURLScraper(
+        args.cid, args.cs)
+    dataframe = reddit_url_scraper.scrap_subreddit(args.subreddit, args.limit)
+    subreddit_utils.DataFrameUtils.write_csv_to_file(
+        args.output, ",", dataframe)
 
 
 main()
---
+++ subreddit_utils.py
@@ -0,0 +1,45 @@
+import praw
+import pandas
+
+
+class SubredditURLScraper():
+    reddit = None
+    post_list = []
+
+    def __init__(self, client_id, client_secret):
+        self.reddit = praw.Reddit(client_id=client_id, client_secret=client_secret,
+                                  user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36')
+        self.reddit.read_only = True
+
+    def scrap_subreddit(self, subreddit: str, limit: int) -> pandas.DataFrame:
+        if limit == -1:
+            posts = self.reddit.subreddit(subreddit).new(limit=None)
+        else:
+            posts = self.reddit.subreddit(subreddit).new(limit=limit)
+
+        counter = 0
+        for post in posts:
+            self.post_list.append([post.title, post.subreddit, post.permalink,
+                                   post.url, post.num_comments, post.selftext])
+            counter = counter + 1
+        data = pandas.DataFrame(
+            self.post_list, columns=['title', 'subreddit', 'reddit_post_url', 'article_linked', 'num_comments', 'body'])
+        print("{0} post have been collected from the {1}".format(
+            counter, subreddit))
+        return data
+
+
+class PostCommentScraper():
+    comments = []
+
+
+class DataFrameUtils():
+
+    @staticmethod
+    def write_csv_to_file(file_name: str, sep: str, dataframe: pandas.DataFrame):
+        try:
+            dataframe.to_csv(file_name, sep=sep)
+            print("Wrote {0} values from dataframe to file [{1}]".format(
+                dataframe.shape[0], file_name))
+        except Exception as e:
+            print(e)
